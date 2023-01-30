57c399825edf52138028b3ff4029c56907c19640
Ng <hcn19@ic.ac.uk>
2023-01-30 12:33:15 +0000

    Ex 2.3 Changing Ratio

--- timers.c
+++ timers.c
@@ -8,7 +8,15 @@ void Timer0_init(void)
 {
     T0CON1bits.T0CS=0b010; // Fosc/4
     T0CON1bits.T0ASYNC=0; // see datasheet errata - needed to ensure correct operation when Fosc/4 used as clock source
-    T0CON1bits.T0CKPS=0b1111; // 1:32768 Prescaler Rate select bit, p359
+    
+    T0CON1bits.T0CKPS=0b1000; // the equation to arrive at 1:244.1 ratio is
+    /*
+     * Frequency = 64 Mhz
+     * Max bin val = 65536
+     * Seconds to max val = max bin val / (clock frequency/4)
+     * We need to divide by 4 because of Fosc/4
+     * Ratio = 1 / seconds to max
+     */
     T0CON0bits.T016BIT=1;	//8bit mode	
 	
     // it's a good idea to initialise the timer registers so we know we are at 0
