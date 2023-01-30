9cc42a11a579e9e999a20408698c60ea349e315f
Ng <hcn19@ic.ac.uk>
2023-01-30 12:50:24 +0000

    Ex 2.4 Timer interrupts

--- interrupts.c
+++ interrupts.c
@@ -10,8 +10,12 @@ void Interrupts_init(void)
 	// turn on global interrupts, peripheral interrupts and the interrupt source 
 	// It's a good idea to turn on global interrupts last, once all other interrupt configuration is done.
     INTCONbits.PEIE = 1; // setting up peripheral interrupts
-    PIE2bits.C1IE = 1; // setting up the interrupt source
+    PIE2bits.C1IE = 1; // setting up the interrupt source p700
     PIR2bits.C1IF = 0; // resetting the flag
+    
+    PIE0bits.TMR0IE = 1; // setting up the interrupt source p700
+    PIR0bits.TMR0IF = 0; // resetting the flag
+    
     IPR2bits.C1IP = 1; // setting it to high priority
     INTCONbits.GIE=1; 	//turn on interrupts globally (when this is off, all interrupts are deactivated)
 
@@ -24,10 +28,19 @@ void Interrupts_init(void)
 void __interrupt(high_priority) HighISR()
 {
 	//add your ISR code here i.e. check the flag, do something (i.e. toggle an LED), clear the flag...
+    //this is the interrupt for the LDR comparator
     if(PIR2bits.C1IF){
         
         LATDbits.LATD7 = !LATDbits.LATD7;
         PIR2bits.C1IF = 0;
+        
+    }
+    
+    // this is the interrupt for the timer
+    if(PIR0bits.TMR0IF){
+        
+        LATDbits.LATD7 = !LATDbits.LATD7;
+        PIR0bits.TMR0IF = 0;
         
     }
 }
